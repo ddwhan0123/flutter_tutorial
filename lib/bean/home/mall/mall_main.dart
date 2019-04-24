f55d696709fc36ea4f9b66c2ef0483354a069434
Jiajie Wang 王佳杰 <jiajie.wang@nio.com>
2019-04-24 14:48:17 +0800

    feat:上传文章的容器

--- lib/bean/home/mall/mall_main.dart
+++ lib/bean/home/mall/mall_main.dart
@@ -49,6 +49,12 @@ class ResultData {
         json['data'].forEach((v) {
           data.add(new MallBannerBean.fromJson(v));
         });
+        //文章类型item
+      } else if (type == 'article') {
+        data = new List<MallArticleBean>();
+        json['data'].forEach((v) {
+          data.add(new MallArticleBean.fromJson(v));
+        });
       }
     }
     haveLine = json['haveLine'];
@@ -66,6 +72,7 @@ class ResultData {
   }
 }
 
+//banner和leftNavigation共用
 class MallBannerBean {
   String img;
   Goto goto;
@@ -102,6 +109,119 @@ class Goto {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['type'] = this.type;
     data['link'] = this.link;
+    return data;
+  }
+}
+
+class MallArticleBean {
+  String pageCode;
+  String title;
+  String headImage;
+  bool isEmployee;
+  String identity;
+  String nickname;
+  String userTags;
+  int userRelation;
+  String articleMiniPicUrl;
+  int effectiveTime;
+  int readCount;
+  bool isNioAuthorized;
+  String accountId;
+  String identityImage;
+  int commentCount;
+  int likeCount;
+  String swcArticleId;
+  ImUser imUser;
+
+  MallArticleBean(
+      {this.pageCode,
+      this.title,
+      this.headImage,
+      this.isEmployee,
+      this.identity,
+      this.nickname,
+      this.userTags,
+      this.userRelation,
+      this.articleMiniPicUrl,
+      this.effectiveTime,
+      this.readCount,
+      this.isNioAuthorized,
+      this.accountId,
+      this.identityImage,
+      this.commentCount,
+      this.likeCount,
+      this.swcArticleId,
+      this.imUser});
+
+  MallArticleBean.fromJson(Map<String, dynamic> json) {
+    pageCode = json['pageCode'];
+    title = json['title'];
+    headImage = json['headImage'];
+    isEmployee = json['isEmployee'];
+    identity = json['identity'];
+    nickname = json['nickname'];
+    userTags = json['userTags'];
+    userRelation = json['userRelation'];
+    articleMiniPicUrl = json['articleMiniPicUrl'];
+    effectiveTime = json['effectiveTime'];
+    readCount = json['readCount'];
+    isNioAuthorized = json['isNioAuthorized'];
+    accountId = json['accountId'];
+    identityImage = json['identityImage'];
+    commentCount = json['commentCount'];
+    likeCount = json['likeCount'];
+    swcArticleId = json['swcArticleId'];
+    imUser =
+        json['imUser'] != null ? new ImUser.fromJson(json['imUser']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['pageCode'] = this.pageCode;
+    data['title'] = this.title;
+    data['headImage'] = this.headImage;
+    data['isEmployee'] = this.isEmployee;
+    data['identity'] = this.identity;
+    data['nickname'] = this.nickname;
+    data['userTags'] = this.userTags;
+    data['userRelation'] = this.userRelation;
+    data['articleMiniPicUrl'] = this.articleMiniPicUrl;
+    data['effectiveTime'] = this.effectiveTime;
+    data['readCount'] = this.readCount;
+    data['isNioAuthorized'] = this.isNioAuthorized;
+    data['accountId'] = this.accountId;
+    data['identityImage'] = this.identityImage;
+    data['commentCount'] = this.commentCount;
+    data['likeCount'] = this.likeCount;
+    data['swcArticleId'] = this.swcArticleId;
+    if (this.imUser != null) {
+      data['imUser'] = this.imUser.toJson();
+    }
+    return data;
+  }
+}
+
+class ImUser {
+  String imId;
+  Null status;
+  String nick;
+  String headImageUrl;
+
+  ImUser({this.imId, this.status, this.nick, this.headImageUrl});
+
+  ImUser.fromJson(Map<String, dynamic> json) {
+    imId = json['imId'];
+    status = json['status'];
+    nick = json['nick'];
+    headImageUrl = json['headImageUrl'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['imId'] = this.imId;
+    data['status'] = this.status;
+    data['nick'] = this.nick;
+    data['headImageUrl'] = this.headImageUrl;
     return data;
   }
 }
---
+++ lib/page/gift/article/mall_article_root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sample/base/widget/base_stateless_widget.dart';
+
+class MallArticleComponent extends BaseStatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+  
+}
