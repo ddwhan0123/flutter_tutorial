a7dba90bf505ecbc3cf5d56bfc7a010397b34863
Jiajie Wang 王佳杰 <jiajie.wang@nio.com>
2019-04-28 19:10:41 +0800

    feat:修改Application

--- android/app/src/main/java/com/example/flutter_sample/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_sample/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.flutter_sample;
 
 import android.os.Bundle;
+
+import java.lang.ref.WeakReference;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
+    public static WeakReference<MainActivity> sRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sRef = new WeakReference<>(this);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sRef.clear();
+        sRef = null;
+    }
+
 }
