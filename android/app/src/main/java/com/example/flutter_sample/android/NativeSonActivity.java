4ec485f6cf201495ae00df3092ed261d242f7e05
Jiajie Wang 王佳杰 <jiajie.wang@nio.com>
2019-04-28 19:04:14 +0800

    feat:上传一些容器

---
+++ android/app/src/main/java/com/example/flutter_sample/android/NativeSonActivity.java
@@ -0,0 +1,14 @@
+package com.example.flutter_sample.android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NativeSonActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
