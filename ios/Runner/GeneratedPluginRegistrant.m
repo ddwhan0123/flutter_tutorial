64cd92c39acb424879279542ca80b8d4a17507fa
Jiajie Wang 王佳杰 <jiajie.wang@nio.com>
2019-04-29 09:48:13 +0800

    feat:完成回滚

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,19 +4,15 @@
 
 #import "GeneratedPluginRegistrant.h"
 #import <flustars/FlustarsPlugin.h>
-#import <flutter_boost/FlutterBoostPlugin.h>
 #import <fluttertoast/FluttertoastPlugin.h>
 #import <shared_preferences/SharedPreferencesPlugin.h>
-#import <xservice_kit/XserviceKitPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FlustarsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlustarsPlugin"]];
-  [FlutterBoostPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBoostPlugin"]];
   [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
   [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
-  [XserviceKitPlugin registerWithRegistrar:[registry registrarForPlugin:@"XserviceKitPlugin"]];
 }
 
 @end
