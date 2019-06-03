c504da1f3c3ade295aef58b8eaf359377e1f6904
Jiajie Wang 王佳杰 <jiajie.wang@nio.com>
2019-06-03 15:04:41 +0800

    feat:添加 缓存 sample

--- lib/page/home/top_moment.dart
+++ lib/page/home/top_moment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sample/base/page/base_stateful_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MomentComponent extends BaseStatefulPage {
   //构造函数传参
@@ -15,7 +16,24 @@ class MomentComponent extends BaseStatefulPage {
 class MomentComponentState extends BasePageState {
   @override
   Widget build(BuildContext context) {
-    return new Text("MomentComponentState");
+    return Center(
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('存储'),
+            onPressed: save,
+          ),
+          RaisedButton(
+            child: Text('读取'),
+            onPressed: _readShared,
+          ),
+          RaisedButton(
+            child: Text('删除'),
+            onPressed: _removeShared,
+          )
+        ],
+      ),
+    );
   }
 
   @override
@@ -26,5 +44,34 @@ class MomentComponentState extends BasePageState {
   @override
   String trackPageEndMethod() {
     return null;
+  }
+
+  /*
+   * 存储数据
+   */
+  Future save() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setString('account', "10086");
+  }
+
+  /*
+   * 读取数据
+   */
+  Future _readShared() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String account = preferences.get('account');
+    if (null != account) {
+      showToast(account);
+    } else {
+      showToast('数据为空');
+    }
+  }
+
+/*
+   * 删除数据
+   */
+  Future _removeShared() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.remove('account');
   }
 }
